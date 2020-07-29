90458e13e89aaecf3cfe620699d6e00bb10eb6db
Anthony Yang <ay339@cornell.edu>
2020-07-29 11:30:28 -0400

    Commit demo

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+
+const ObjectId = mongodb.ObjectID;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+async function prepareDb() {
+    let collection;
+    let db;
+    const client = await mongodb.MongoClient.connect('mongodb://localhost:27017/',{
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+    try {
+        await client.connect();
+        db = client.db("zoom");
+        try {
+            collection = await db.createCollection('ids');
+        } catch {
+            console.log("Collection already exists");
+            collection = await db.collection('ids');
+        }
+        console.log("Collection ready");
+    } catch (e) {
+        console.log(e, false);
+        return;
+    }
+
+    app.get('/session/:id', async (req, res) => {
+        const id = req.params.id;
+        const doc = await collection.findOneAndUpdate({
+            _id: new ObjectId(id)
+        }, {
+            $inc: {
+                seq: 1
+            }
+        });
+        const value = doc.value;
+        return res.send({
+            url: `${value.url}&seq=${value.seq}&lang=${value.lang}`
+        });
+    });
+
+    app.post('/session/create', async (req, res) => {
+        const token = req.body.token;
+        const doc = await collection.findOne({
+            url: token
+        })
+        if (doc != null) {
+            return res.status(200).send({
+                message: "Already exists",
+                id: doc._id
+            });
+        } else {
+            const doc = await collection.insertOne({
+                url: token,
+                seq: 1,
+                lang: 'en-US'
+            });
+            return res.status(201).send(
+                {
+                    message: "Session created",
+                    id: doc.insertedId
+                });
+        }
+    });
+
+    app.listen(8080, () => {
+        console.log(true);
+        console.log("Listening on port 8080");
+    }).on('close', (err) => {
+        db != null && db.close();
+    })
+}
+
+prepareDb()
